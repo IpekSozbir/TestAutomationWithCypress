ca10185a5ae36ba6a4eefda306a91be3cf91fdf1
IpekSozbir <ipekoren@gmail.com>
2025-09-10 21:14:39 +0300

    v2

---
+++ cypress/e2e/testAutomationWithCypress/day02/day02_c01_test01.cy.js
@@ -0,0 +1,10 @@
+context('context kullanimi', () => {
+    it('google ziyaret', () => {
+        cy.visit('http://google.com')
+    });
+
+    it('title', () => {
+        cy.visit('http://google.com')
+        cy.title().should('include','Google')
+    });
+});
