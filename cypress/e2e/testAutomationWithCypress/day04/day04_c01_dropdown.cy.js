f72a5f7330c9c9b109e4b8c413092fc572943eab
IpekSozbir <ipekoren@gmail.com>
2025-09-29 22:35:21 +0300

    v15

---
+++ cypress/e2e/testAutomationWithCypress/day04/day04_c01_dropdown.cy.js
@@ -0,0 +1,12 @@
+ ///<reference types="Cypress" /> // komutlarin hemen gelmesi icin
+ 
+ describe('Dropdown Locate', () => {
+
+    it('Dropdown Menu', () => {
+        cy.visit('https://the-internet.herokuapp.com/dropdown')
+        cy.get('#dropdown').select('Option 1').should('have.value','1') 
+        cy.get('#dropdown').select('Option 2').should('have.value','2') 
+        // dropdown webElemnetlerde assertion islemi icin have.text gibi have.value kullanabiliriz
+    });
+
+ });
