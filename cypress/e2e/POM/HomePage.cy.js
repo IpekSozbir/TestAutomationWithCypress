d96fcae04ac12231de8f053ac1c2f91bd63376de
IpekSozbir <ipekoren@gmail.com>
2025-10-30 12:51:16 +0300

    v37

---
+++ cypress/e2e/POM/HomePage.cy.js
@@ -0,0 +1,11 @@
+class HomePage{
+    getLoginLink(){
+       return cy.contains('login',{matchCase:false});
+    }
+
+    getVisit(){
+        return cy.visit('http://automationexercise.com');
+    }
+
+}
+export default HomePage // diger dosyalardan ulasabilmek icin
