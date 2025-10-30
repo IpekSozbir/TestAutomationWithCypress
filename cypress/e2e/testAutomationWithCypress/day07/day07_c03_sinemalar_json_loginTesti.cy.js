9aeb85c525676031b98f39d59046e4746c75a171
IpekSozbir <ipekoren@gmail.com>
2025-10-30 12:43:55 +0300

    v36

--- cypress/e2e/testAutomationWithCypress/day07/day07_c03_sinemalar_json_loginTesti.cy.js
+++ cypress/e2e/testAutomationWithCypress/day07/day07_c03_sinemalar_json_loginTesti.cy.js
@@ -12,10 +12,14 @@ before(function () {
 
     it('Json Login Testi', function() {
         cy.visit(this.data.url)
+        cy.handleOptionalPopups()
         cy.get('.account > .icon-only').click()
         cy.contains('giriş yap',{matchCase:false}).click()
         cy.get('#login-username').type(this.data.username)
         cy.get('#login-password').type(this.data.password)
+        cy.get('#login-form > .btn.primary').click()
+        cy.get('[menu-id="account-nav"] > .img-container > img').click()
+        cy.get('.btn-list > :nth-child(1) > .btn').should('be.visible')
         
     });
 });
